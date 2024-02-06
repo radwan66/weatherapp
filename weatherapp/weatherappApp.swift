49502eccfa8514bcd366ec785c2d3011281471a7
radwan <radwan@192.168.1.23>
2024-02-06 14:14:37 +0200

    first commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by radwan on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
